9d838f587488964539e863d041bb07c61af120b8
DavidR26 <dawffydd_r10@hotmail.com>
2026-08-12 23:25:12 -0600

    Se termina script de carga. Falta probar la carga a la base de datos de postgresql

--- spark_scripts/Carga.py
+++ spark_scripts/Carga.py
@@ -1,56 +1,125 @@
+import os
+from dotenv import load_dotenv
 import logging
 from pathlib import Path
-
-import pandas as pd
-from sqlalchemy import create_engine,text
-from sqlalchemy.exc import SQLAlchemyError
-from pyspark.sql import DataFrame
-from pyspark.sql import SparkSession
-
-
+from pyspark.sql import DataFrame,SparkSession
+from pyspark.sql import Row
 
 
 class CargaDatos:
     
-    def __init__(self,cadenaConexion: str, db_config: dict):
+    def __init__(self,spark: SparkSession,cadenaConexion, usuario,password):
+        self.spark = spark
         self.cadenaConexion=cadenaConexion
-        
+        self.usuario=usuario
+        self.password=password
         #self.motor=create_engine(cadenaConexion)
 
     def test_conexion(self):
         try:
-            with self.motor.connect() as conexion:
-                resultado=conexion.execute(text("Select 1"))
-                print("Conexion exitosa a Postgres")
-        except SQLAlchemyError as e:
-                print("Error al conectar a Postgres")
-                print(e)
-    def cargar_a_sql(self,df,nombre_tabla,modo='replace'):
-        if df.empty:
+            (
+                self.spark.read
+                .format("jdbc")
+                .option("url", self.cadenaConexion)
+                .option("dbtable", "(SELECT 1) AS prueba")
+                .option("user", self.usuario)
+                .option("password", self.password)
+                .option("driver", "org.postgresql.Driver")
+                .load()
+                .show()
+            )
+
+            logging.info("Conexión exitosa a postgres.")
+
+        except Exception as e:
+
+            logging.error(f"Error al conectar con postgres: {e}")
+
+            raise
+
+
+    def cargar_a_sql(self,df:DataFrame,nombre_tabla:str,modo:str= "overwrite"): #modo='overwrite'
+        if df.rdd.isEmpty():
             print("El DataFrame está vacío. No se cargó nada.")
+            logging.warning("El DataFrame está vacío. No se realizará la carga.")
             return
-        if modo not in ['replace', 'append']:
-            print(f"Modo '{modo}' no válido. Usa 'replace' o 'append'.")
-            return
+        if modo not in ["overwrite", "append"]:
+            print(f"El modo debe ser 'overwrite' o 'append")
+            raise ValueError("El modo debe ser 'overwrite' o 'append'.")
+            
     
         try:
-            df.to_sql(nombre_tabla,con=self.motor,if_exists=modo,index=False)
-            print(f"Datos cargados en la tabla '{nombre_tabla}' usando modo '{modo}'")
-        except SQLAlchemyError as e:
-            print("Error al cargar datos a SQL Server:")
-            print(e)
+
+            (
+                df.write
+                .format("jdbc")
+                .option("url", self.cadenaConexion)
+                .option("dbtable", nombre_tabla)
+                .option("user", self.usuario)
+                .option("password", self.password)
+                .option("driver", "org.postgresql.Driver")
+                .mode(modo)
+                .save()
+            )
+
+            logging.info(f"Datos cargados correctamente en '{nombre_tabla}'.")
+
+        except Exception as e:
+
+            logging.error(f"Error al cargar datos en PostgreSQL: {e}")
+
+            raise
 
 if __name__ == "__main__":    
-    
-    print("De nuevo please")
+    logging.basicConfig(level=logging.INFO)
+    print("fase de carga")
     spark = (
         SparkSession.builder
-        .appName("TransformacionPeliculas")
+        .appName("CargaPeliculas")
+        .config("spark.jars", "/opt/spark/jars/postgresql-42.7.3.jar")
         .getOrCreate()
     )
 
+    load_dotenv(dotenv_path=Path("/opt/spark/.env"))
+    print("HOST:", os.getenv("POSTGRES_HOST"))
+  
 
 
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
+    print("HOST:", POSTGRES_HOST)
+    print("PORT:", POSTGRES_PORT)
+    print("DB:", POSTGRES_DB)
+    print("USER:", POSTGRES_USER)
+    print("PWD:", POSTGRES_PASSWORD)
+
+
+    CADENA_CONEXION = (
+    f"jdbc:postgresql://"
+    f"{POSTGRES_HOST}:"
+    f"{POSTGRES_PORT}/"
+    f"{POSTGRES_DB}") 
+
+    cargador = CargaDatos(spark, CADENA_CONEXION, POSTGRES_USER, POSTGRES_PASSWORD)
+    cargador.test_conexion()
+
+# Mock DataFrame
+    datos_mock = [
+    Row(id=1, titulo="Matrix", anio=1999),
+    Row(id=2, titulo="Inception", anio=2010),
+    Row(id=3, titulo="Interstellar", anio=2014),
+    ]   
+    df_mock = spark.createDataFrame(datos_mock)
+
+    cargador.cargar_a_sql(df_mock, "peliculas_mock", modo="overwrite")
+
+
+
+    spark.stop()
 """
     # Validación del dataframe
     
